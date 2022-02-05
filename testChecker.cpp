41aa7504b874334b8bc0837f48343ea507de346d
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-02-05 17:40:39 +0530

    TestCases for checking the functionality in checker.h

--- testChecker.cpp
+++ testChecker.cpp
@@ -21,8 +21,20 @@ void testBatteryCheck(float testTemperature, float testsoc, float testChargeRate
     assert(batteryIsOk(testTemperature,testsoc,testChargeRate)==expectedResult);
 }
 
-void testAnyDataOutOfRange(float testTemperature, float testsoc, float testChargeRate, bool expectedResult) {
-    assert(checkAnyDataOutOfRange(testTemperature,testsoc,testChargeRate)==expectedResult);
+void testAnyDataOutOfRange(float testData, float testsoc, float testChargeRate, bool expectedResult) {
+    assert(checkAnyDataOutOfRange(testData,testsoc,testChargeRate)==expectedResult);
+}
+
+void testdataBelowLowerLimit(float testData, float lowerLimit, string dataType,bool expectedResult){
+    assert(isDataBelowLowerLimit(testData,lowerLimit,dataType)==expectedResult);
+}
+
+void testdataAboveUpperLimit(float testData, float upperLimit, string dataType,bool expectedResult){
+    assert(isDataAboveUpperLimit(testData,upperLimit,dataType)==expectedResult);
+}
+
+void testDataOutOfLimit(float testData,float limit,string dataType,bool (*dataOutOfLimitCheckFunction)(float,float,string), bool expectedResult) {
+  assert(isDataOutOfLimit(testData,limit,dataType,*dataOutOfLimitCheckFunction)==expectedResult);
 }
 
 int main() {
@@ -40,6 +52,10 @@ int main() {
   testAnyDataOutOfRange(25, 70, 0.7, false); //all the date are within limit
   testAnyDataOutOfRange(40, 70, 2.0, true); //charge rate greater than 0.8, so battery not ok
   testBatteryCheck(25, 70, 0.7, true); //all the date are within limit
+  testdataBelowLowerLimit(-5,0,"Temperature",true);//temperature lower than 0, so temperature out of lower limit
+  testdataAboveUpperLimit(85,80,"State of Charge",true);//soc greater than 80, so soc out of upper limit
+  testdataAboveUpperLimit(0.5,0.8,"Charge Rate",false);//charge rate below the upper limit
+  testDataOutOfLimit(50,0,"Temperature",&isDataBelowLowerLimit,false);//temperature more than 0
   testBatteryCheck(50, 80, 0, false); //temperature greater than 45, so battery not ok
   testBatteryCheck(30, 90, 0, false); //soc greater than 80, so battery not ok
   testBatteryCheck(30, 90, 1.0, false); //charge rate greater than 0.8, so battery not ok
