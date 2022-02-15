477a1296ba9f2980d259ed2400dea1be5545abc6
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-02-16 00:42:40 +0530

    Update testChecker.cpp

--- testChecker.cpp
+++ testChecker.cpp
@@ -1,62 +1,103 @@
-#include <assert.h>
-#include "checker.h"
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
 
-void testOutOfRange(float testData, bool (*OutOfRangeCheckFunction)(float), bool expectedResult) {
-  assert(isOutOfRange(testData,*OutOfRangeCheckFunction)==expectedResult);
-}
-   
-void testTemperatureOutOfRange(float testTemperature, bool expectedResult) {
-   assert(isTemperatureOutOfRange(testTemperature)==expectedResult);
+
+#define LOWER_TEMPERATURE_LIMIT 0
+#define UPPER_TEMPERATURE_LIMIT 45
+#define LOWER_CHARGE_LIMIT 20
+#define UPPER_CHARGE_LIMIT 80
+#define LOWER_CHARGE_RATE 0.0
+#define UPPER_CHARGE_RATE 0.8
+#define WARNING_TOLERANCE_VALUE 0.05
+
+enum supportedLanguage
+{
+  ENGLISH,
+  GERMAN
+};
+
+supportedLanguage outputLanguage=ENGLISH;
+enum warningLevel
+{
+   LOW_BREACH_LEVEL,
+   LOW_WARNING_LEVEL,
+   NORMAL_LEVEL,
+   HIGH_WARNING_LEVEL,
+   HIGH_BREACH_LEVEL
+};
+
+std::vector<std::string> warningMessageInEnglish={"Warning: Below lower limit","Warning: Approaching low breach level","Normal","Warning: Approaching high breach level","Warning: Above high level"};
+std::vector<std::string> warningMessageInGerman={"Warnung: Untere Grenze unterschritten","Warnung: Annäherung an niedrige Verletzungsstufe","Normal","Warnung: Annäherung an hohe Verletzungsstufe","Warnung: Über dem hohen Niveau"};
+std::vector<std::string> dataTypeInEnglish={"Temperature ","SOC ","Charge Rate "};
+std::vector<std::string> dataTypeInGerman={"Temperatur ","SOC ","Ladestrom "};
+
+std::map<supportedLanguage,std::vector<std::string>> warningMessageList ={{ENGLISH,warningMessageInEnglish},{GERMAN,warningMessageInGerman}};
+std::map<supportedLanguage,std::vector<std::string>> dataTypeList={{ENGLISH,dataTypeInEnglish},{GERMAN,dataTypeInGerman}};
+
+float getToleranceValue(float upperLimit){
+    return (WARNING_TOLERANCE_VALUE*upperLimit);
 }
 
-void testStateOfChargeOutOfRange(float testChargeData, bool expectedResult) {
-   assert(isStateOfChargeOutOfRange(testChargeData)==expectedResult);
+enum warningLevel getWarningLevelStatus(float data, float lower_limit,float upper_limit){
+   if(data<=lower_limit+getToleranceValue(upper_limit))
+   {
+       return ((data<=lower_limit)?LOW_BREACH_LEVEL:LOW_WARNING_LEVEL);
+   }
+   else if(data>=upper_limit-getToleranceValue(upper_limit))
+   {
+       return ((data>upper_limit)?HIGH_BREACH_LEVEL:HIGH_WARNING_LEVEL);
+   }
+   return NORMAL_LEVEL;
 }
 
-void testChargeRateOutOfRange(float testChargeRate, bool expectedResult) {
-   assert(isChargeRateOutOfRange(testChargeRate)==expectedResult);
+void printWarningMessage(string stringToPrint){
+    cout << stringToPrint;
 }
 
-void testBatteryCheck(float testTemperature, float testsoc, float testChargeRate, bool expectedResult) {
-    assert(batteryIsOk(testTemperature,testsoc,testChargeRate)==expectedResult);
+bool isDataOutOfLimit (float data,float limit,string dataType,bool (*dataOutOfLimitCheckFunction)(float,float,string)) {
+    return dataOutOfLimitCheckFunction(data,limit,dataType);
 }
 
-void testAnyDataOutOfRange(float testData, float testsoc, float testChargeRate, bool expectedResult) {
-    assert(checkAnyDataOutOfRange(testData,testsoc,testChargeRate)==expectedResult);
+bool isTemperatureOutOfRange(float temperature) {
+    warningLevel temperatureWarningStatus=getWarningLevelStatus(temperature,LOWER_TEMPERATURE_LIMIT,UPPER_TEMPERATURE_LIMIT);
+    printWarningMessage(dataTypeList[outputLanguage][0]+warningMessageList[outputLanguage][temperatureWarningStatus]+"\n");
+    if(temperatureWarningStatus==LOW_BREACH_LEVEL || temperatureWarningStatus==HIGH_BREACH_LEVEL)
+	{
+	  return true;
+	}
+	return false;
 }
 
-void testdataBelowLowerLimit(float testData, float lowerLimit, string dataType,bool expectedResult){
-    assert(isDataBelowLowerLimit(testData,lowerLimit,dataType)==expectedResult);
+bool isStateOfChargeOutOfRange(float soc) {
+    warningLevel socWarningStatus=getWarningLevelStatus(soc,LOWER_CHARGE_LIMIT,UPPER_CHARGE_LIMIT);
+    printWarningMessage(dataTypeList[outputLanguage][1]+warningMessageList[outputLanguage][socWarningStatus]+"\n");
+    if(socWarningStatus==LOW_BREACH_LEVEL || socWarningStatus==HIGH_BREACH_LEVEL)
+	{
+	  return true;
+	}
+	return false;
 }
 
-void testdataAboveUpperLimit(float testData, float upperLimit, string dataType,bool expectedResult){
-    assert(isDataAboveUpperLimit(testData,upperLimit,dataType)==expectedResult);
+bool isChargeRateOutOfRange(float chargeRate) {
+    warningLevel chargeRateWarningStatus=getWarningLevelStatus(chargeRate,LOWER_CHARGE_RATE,UPPER_CHARGE_RATE);
+    printWarningMessage(dataTypeList[outputLanguage][2]+warningMessageList[outputLanguage][chargeRateWarningStatus]+"\n");
+    if(chargeRateWarningStatus==LOW_BREACH_LEVEL || chargeRateWarningStatus==HIGH_BREACH_LEVEL)
+	{
+	  return true;
+	}
+	return false;
 }
 
-void testDataOutOfLimit(float testData,float limit,string dataType,bool (*dataOutOfLimitCheckFunction)(float,float,string), bool expectedResult) {
-  assert(isDataOutOfLimit(testData,limit,dataType,*dataOutOfLimitCheckFunction)==expectedResult);
+bool isOutOfRange (float data,bool (*OutOfRangeCheckFunction)(float)) {
+    return OutOfRangeCheckFunction(data);
 }
 
-int main() {
-  testTemperatureOutOfRange(-10, true); //temperature lower than 0, so temperature out of range
-  testTemperatureOutOfRange(50, true);  //temperature greater than 45, so temperature out of range
-  testTemperatureOutOfRange(30, false);  //temperature is not out of range
-  testStateOfChargeOutOfRange(10, true); //soc lower than 20, so soc out of range
-  testStateOfChargeOutOfRange(90, true);  //soc greater than 80, so soc out of range
-  testStateOfChargeOutOfRange(40, false);  //soc is not out of range
-  testChargeRateOutOfRange(1.5, true); //charge rate greater than 0.8, so out of range
-  testChargeRateOutOfRange(0.5, false); //charge rate less than 0.8
-  testOutOfRange(70,&isTemperatureOutOfRange,true); //temperature greater than 45, so temperature out of range
-  testOutOfRange(100,&isStateOfChargeOutOfRange,true); //soc greater than 80, so soc out of range
-  testOutOfRange(1.0,&isChargeRateOutOfRange,true); //charge rate greater than 0.8, so out of range
-  testAnyDataOutOfRange(25, 70, 0.7, false); //all the date are within limit
-  testAnyDataOutOfRange(40, 70, 2.0, true); //charge rate greater than 0.8, so battery not ok
-  testBatteryCheck(25, 70, 0.7, true); //all the date are within limit
-  testdataBelowLowerLimit(-5,0,"Temperature",true);//temperature lower than 0, so temperature out of lower limit
-  testdataAboveUpperLimit(85,80,"State of Charge",true);//soc greater than 80, so soc out of upper limit
-  testdataAboveUpperLimit(0.5,0.8,"Charge Rate",false);//charge rate below the upper limit
-  testDataOutOfLimit(50,0,"Temperature",&isDataBelowLowerLimit,false);//temperature more than 0
-  testBatteryCheck(50, 80, 0, false); //temperature greater than 45, so battery not ok
-  testBatteryCheck(30, 90, 0, false); //soc greater than 80, so battery not ok
-  testBatteryCheck(30, 90, 1.0, false); //charge rate greater than 0.8, so battery not ok
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  bool isBatteryOK=true;
+  if(isOutOfRange(temperature,&isTemperatureOutOfRange)||isOutOfRange(soc,&isStateOfChargeOutOfRange) || isOutOfRange(chargeRate,&isChargeRateOutOfRange)) {
+    isBatteryOK= false;
+  }
+  return isBatteryOK;
 }
